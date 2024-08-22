323cb60a61e85aadb28fdae401832cededdbc89c
Melker Dahl <jaghetermelker@gmail.com>
2024-08-22 14:09:24 +0200

    start

---
+++ db.js
@@ -0,0 +1,15 @@
+import mysql from "mysq12/promise.js";
+
+// Create a pool of database connections
+const pool = mysql.createPool({
+  host: "localhost",
+  port: "3306",
+  user: "root",
+  password: "",
+  database: "gritacademy",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+export default pool;
