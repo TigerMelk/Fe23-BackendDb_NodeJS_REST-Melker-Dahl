420cb074c15d3b4146787bb3c3dc8d4777e0d597
Melker Dahl <jaghetermelker@gmail.com>
2024-08-25 22:18:43 +0200

    start

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/", async (req, res) => {
+  res.render("index");
+});
+app.get("/students", async (req, res) => {
+  let sql = "SELECT * FROM students";
+  const dbData = await db.query(sql);
+  res.render("students", { dbData });
+});
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
