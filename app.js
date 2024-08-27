13c534603e66d5a640ec05319fbb1195b9c67114
TigerMelk <96880867+TigerMelk@users.noreply.github.com>
2024-08-27 11:38:03 +0200

    fixed

--- app.js
+++ app.js
@@ -10,13 +10,14 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
-  res.render("index");
+	res.render("index");
 });
 app.get("/students", async (req, res) => {
-  let sql = "SELECT * FROM students";
-  const dbData = await db.query(sql);
-  res.render("students", { dbData });
+	let sql = "SELECT * FROM students";
+	const dbData = await db.query(sql);
+	console.dir(dbData);
+	res.render("students", { dbData });
 });
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+	console.log("Server is running on port 3000");
 });
