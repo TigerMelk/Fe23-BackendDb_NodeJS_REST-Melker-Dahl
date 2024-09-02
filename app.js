055186ffeccc12e8e2252c795c141b64ac23a0f6
Melker Dahl <jaghetermelker@gmail.com>
2024-09-02 16:49:34 +0200

    update.
    add course add student delete.

--- app.js
+++ app.js
@@ -35,24 +35,28 @@ app.get("/students/:studentId", async (req, res) => {
   let sql = `
 	SELECT students.fName, students.lName, courses.name as courseName, courses.description 
 	FROM students
-	JOIN student_courses ON students.id = student_courses.student_id
-	JOIN courses ON student_courses.course_id = courses.id
+	LEFT JOIN student_courses ON students.id = student_courses.student_id
+	LEFT JOIN courses ON student_courses.course_id = courses.id
 	WHERE students.id = ?`;
 
   const studentData = await db.query(sql, [studentId]);
 
-  if (studentData.length > 0) {
-    const studentDetails = {
-      fName: studentData[0].fName,
-      lName: studentData[0].lName,
-    };
-    res.render("student-details", {
-      student: studentDetails,
-      courses: studentData,
-    });
-  }
+  let coursesSql = `SELECT id, name FROM courses`;
+  const dbCourses = await db.query(coursesSql);
+
+  const enrolledCourses = studentData.filter(
+    (course) => course.courseName !== null
+  );
+
+  res.render("student-details", {
+    student: { fName: studentData[0].fName, lName: studentData[0].lName },
+    courses: enrolledCourses,
+    dbCourses: dbCourses,
+    studentId: studentId,
+  });
 });
 
+// courses
 app.get("/courses", async (req, res) => {
   let sql = "SELECT * FROM courses";
   const dbData = await db.query(sql);
@@ -60,6 +64,7 @@ app.get("/courses", async (req, res) => {
   res.render("courses", { dbData });
 });
 
+// courses - details
 app.get("/courses/:courseId", async (req, res) => {
   const courseId = req.params.courseId;
   let sql = `SELECT courses.name AS courseName, 
@@ -86,6 +91,7 @@ app.get("/courses/:courseId", async (req, res) => {
   res.render("course-details", { course: courseDetails });
 });
 
+// courses students association
 app.get("/courses-students", async (req, res) => {
   let sql = `
 	  SELECT courses.id AS courseId,
@@ -122,6 +128,83 @@ app.get("/courses-students", async (req, res) => {
 
   const coursesArray = Object.values(courses);
   res.render("courses-students", { courses: coursesArray });
+});
+
+// Add student
+app.get("/addStudent", async (req, res) => {
+  let sql = "SELECT id, name FROM courses";
+  const dbCourses = await db.query(sql);
+  res.render("addStudent", { dbCourses });
+});
+
+app.post("/addStudent", async (req, res) => {
+  const { fName, lName, town, courseId } = req.body;
+
+  let addStudentSql = `INSERT INTO students (fName, lName, town) 
+  VALUES (?,?,?)`;
+
+  const result = await db.query(addStudentSql, [fName, lName, town]);
+
+  const newStudentId = result.insertId;
+
+  let addCourseAssSql = `INSERT INTO student_courses (student_id, course_id) 
+  VALUES (?, ?)`;
+  await db.query(addCourseAssSql, [newStudentId, courseId]);
+  res.redirect("/students");
+});
+
+// Add course
+app.get("/addCourse", async (req, res) => {
+  res.render("addCourse");
+});
+
+app.post("/addCourse", async (req, res) => {
+  const { name, description } = req.body;
+
+  let sql = `INSERT INTO courses (name, description) 
+	VALUES (?,?)`;
+  await db.query(sql, [name, description]);
+  res.redirect("/courses");
+});
+
+// Enroll existing student into course
+app.post("/enrollStudent", async (req, res) => {
+  const { studentId, courseId } = req.body;
+
+  if (!studentId || !courseId) {
+    return res.status(400).send("student id and course id are req");
+  }
+  let sql = `INSERT INTO student_courses (student_id, course_id) 
+	VALUES (?,?)`;
+
+  await db.query(sql, [studentId, courseId]);
+  res.redirect(`/students/${studentId}`);
+});
+
+// delete a student and association
+app.post("/deleteStudent", async (req, res) => {
+  const { studentId } = req.body;
+
+  let deleteAssSql = `DELETE FROM student_courses WHERE student_id = ?`;
+  await db.query(deleteAssSql, [studentId]);
+
+  let deleteStudentSql = `DELETE FROM students WHERE id = ?`;
+  await db.query(deleteStudentSql, [studentId]);
+
+  res.redirect("/students");
+});
+
+// delete course
+app.post("/deleteCourse", async (req, res) => {
+  const { courseId } = req.body;
+
+  let deleteAssSql = `DELETE FROM student_courses WHERE course_id = ?`;
+  await db.query(deleteAssSql, [courseId]);
+
+  let deleteCourseSql = `DELETE FROM courses WHERE id = ?`;
+  await db.query(deleteCourseSql, [courseId]);
+
+  res.redirect("/courses");
 });
 
 app.listen(3000, () => {
